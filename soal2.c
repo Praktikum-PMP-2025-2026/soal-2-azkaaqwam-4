8874ffa535c0493095b401956b1ef45622053b1b
azkaaqwam <azkamhd08@gmail.com>
2026-05-04 09:09:48 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,8 +1,1 @@
-/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
- *   Modul               : 4 - Dynamic Structures
- *   Hari dan Tanggal    : Senin,4 Mei 2026
- *   Nama (NIM)          : Azka Muhammad Aqwam(13224061)
- *   Nama File           : soal2.c
- *   Deskripsi           : Dengan menggunakan dynamic structures membuat program yang menggabungkan 2 linkedlist yang berbeda
- * 
- */
+
